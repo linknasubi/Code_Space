84131600fc4b43f0a68209773e78be43f72fcb8b
Gabriel Oliveira Aragão <linknasubi2@gmail.com>
2020-11-04 01:54:54 -0300

    Added responsive canvas

--- js/index.js
+++ js/index.js
@@ -9,27 +9,29 @@ var mouse_click = [{x:0, y:0}];
 
 
 // setup config variables and start the program
-function init () {
+function init() {
 	
 	
 
 	// set our config variables
 	canvas = document.getElementById('gameCanvas')
+	canvas.width = window.innerWidth;
 	var heightRatio = 0.5;
 	canvas.height = canvas.width * heightRatio;
+
 	ctx = canvas.getContext('2d')
 	ctx.globalCompositeOperation = 'destination-over';
 	window.requestAnimationFrame(draw);
 
 }
 
-init()
+init();
 
 
 function distance(xo, x, yo, y, radius, i){
 	dist = Math.sqrt(((x-xo) ** 2) + ((y - yo) ** 2))
 
-	if (dist < radius){
+	if (dist <= radius*1.1){
 		circle_nodes[i].color = "green";
 	}
 
@@ -39,8 +41,8 @@ function distance(xo, x, yo, y, radius, i){
 
 canvas.addEventListener('click', (e) => {
 	pos = {
-	  x: e.clientX,
-	  y: e.clientY
+	  x: e.clientX-5,
+	  y: e.clientY-15
 	};
 
 	for(var i = 0; i < circle_nodes.length; i++){
@@ -91,8 +93,8 @@ function Circle(xPos, yPos){
 	this.x = xPos;
 	this.y = yPos;
 	this.velocity = sign * 0.005;
-	this.radius = Math.round(Math.random()*(30-20) + 20);
-	this.range = Math.random()/10
+	this.radius = Math.round(Math.random()*((canvas.width/60)-(canvas.width/90)) + (canvas.width/90));
+	this.range = Math.random()/8
 	this.radians = 0
 	this.color = 'gray';
 	
@@ -105,6 +107,16 @@ Circle.prototype.update = function(){
 	
 
 	this.radians += this.velocity;
+
+	
+
+	if(this.radians > 20){
+		this.velocity *= -1;
+	}
+
+	if(this.radians < -20){
+		this.velocity *= -1;
+	}
 
 	ctx.beginPath();
 	ctx.fillStyle = this.color;
@@ -125,12 +137,15 @@ Circle.prototype.update = function(){
 function draw_circle(){
 
 	number_points = 10
-	
+	var rel_distance_x = [-(canvas.width/6.4), -(canvas.width/6.4)]
+	var rel_distance_y = [-(canvas.width/6.4), -(canvas.width/6.4)]
+
 	for(var i = 0; i < number_points; i++){
-		sectionXo = ((canvas.width-200)*(i))/number_points;
-		sectionX = ((canvas.width-200)*(i+1)/number_points);
-		sectionYo = ((canvas.height-200)*(number_points-i+1)/number_points);
-		sectionY = ((canvas.height-200)*(number_points-i)/number_points);
+
+		sectionXo = ((canvas.width+rel_distance_x[0])*(i+1))/number_points;
+		sectionX = ((canvas.width+rel_distance_x[1])*(i+2)/number_points);
+		sectionYo = ((canvas.height+rel_distance_y[0])*(number_points-i+2)/number_points);
+		sectionY = ((canvas.height+rel_distance_y[1])*(number_points-i+1)/number_points);
 		randomX = Math.round(Math.random()*(sectionX - sectionXo) + sectionXo);
 		randomY = Math.round(Math.random()*(sectionY - sectionYo) + sectionYo);
 		var circle = new Circle(randomX, randomY);
